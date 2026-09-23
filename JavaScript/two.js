5a792afd234bb20bbf8ba96788a19f1cb5eed865
karan <rathodkaransingh05@gmail.com>
2026-09-23 22:23:33 +0530

    added two files

---
+++ JavaScript/two.js
@@ -0,0 +1,20 @@
+{
+    var a = 100;
+    var b = 200;
+}
+console.log("first " ,a)
+console.log("first ",b)
+
+{
+    let x = 1000;
+    let y = 2000;
+}
+console.log("Second ", x)
+console.log("second ", y)
+
+{
+    const m = 3.14;
+    const n = 3.14;
+}
+console.log("Third " , m)
+console.log("Third ",n)
